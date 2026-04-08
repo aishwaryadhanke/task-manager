31ce0277552e704c81372387d4651d016cebd631
Aishwarya <your-email@gmail.com>
2026-04-08 08:11:05 +0000

    fix url id type for mongo

--- backend/backend_project/tasks/urls.py
+++ backend/backend_project/tasks/urls.py
@@ -3,6 +3,6 @@ from .views import get_tasks, task_detail, register
 
 urlpatterns = [
     path('tasks/', get_tasks),
-    path('tasks/<int:id>/', task_detail),
+    path('tasks/<str:id>/', task_detail),  # ✅ FIXED HERE
     path('register/', register),
 ]
